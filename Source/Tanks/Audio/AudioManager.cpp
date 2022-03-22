8d7245f2f42a585e95f5111f790da8b172205aa0
Michal <michal.niemczykit@gmail.com>
2022-03-22 20:23:27 +0100

    Added audio manager.

---
+++ Source/Tanks/Audio/AudioManager.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AudioManager.h"
+
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+UAudioManager::UAudioManager()
+{
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>("Audio Component");
+}
+
+UWorld* UAudioManager::GetWorld() const
+{
+	return GetOuter()->GetWorld();
+}
+
+void UAudioManager::PlayBackgroundMusic()
+{
+	AudioComponent->SetSound(BackgroundMusic);
+	AudioComponent->FadeIn(BackgroundMusicFadeInDuration);
+}
+
+void UAudioManager::StopBackgroundMusic()
+{
+	AudioComponent->FadeOut(BackgroundMusicFadeOutDuration, 0.f);
+}
+
+void UAudioManager::PlayClockSound()
+{
+	if (CountdownSound)
+	{
+		UGameplayStatics::PlaySound2D(this, CountdownSound);
+	}
+}
+
+void UAudioManager::PlayCountdownEndSound()
+{
+	if (CountdownEndSound)
+	{
+		UGameplayStatics::PlaySound2D(this, CountdownEndSound);
+	}
+}
+
+
---
+++ Source/Tanks/Audio/AudioManager.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AudioManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class TANKS_API UAudioManager : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	void PlayBackgroundMusic();
+	void StopBackgroundMusic();
+
+	void PlayClockSound();
+	void PlayCountdownEndSound();
+
+protected:
+	UAudioManager();
+	virtual UWorld* GetWorld() const override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BackgroundMusicFadeInDuration = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BackgroundMusicFadeOutDuration = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* CountdownEndSound = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAudioComponent* AudioComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* CountdownSound = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* BackgroundMusic = nullptr;
+};
